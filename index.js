62dc2ebd520f64fa6ea52f0319cc15625a074a0d
Vahe227 <vxachatryan650@gmail.com>
2025-10-26 16:23:10 +0400

    for auth-app-server repo

--- index.js
+++ index.js
@@ -25,44 +25,74 @@ const pool = new Pool({
     port: process.env.DB_PORT
 });
 
+pool.on('error', (err, client) => {
+    console.error('DataBase Have An Error: ', err);
+});
+
 server.get('/', (req,res) => {
     res.status(200).send('<h1>Server Code is working!</h1>');
 });
 
 server.post('/isLogined', async (req,res) => {
-    const { result } = req.body;
-    const resultOfTokenTime = await checkTokenTime(jwt, secret, result, pool);
-    if(resultOfTokenTime === 'notExictedTime') {
-        const resultOfFunc = await isUserLogined(pool, result);
-        if(resultOfFunc === 'DontExists') {
+    try {
+        const { result } = req.body;
+        const resultOfTokenTime = await checkTokenTime(jwt, secret, result, pool);
+        if(resultOfTokenTime === 'notExictedTime') {
+            const resultOfFunc = await isUserLogined(pool, result);
+            if(resultOfFunc === 'DontExists') {
+                return res.json({ state: 'UserDidntExists' });
+            } else if (resultOfFunc === 'Exists') {
+                return res.json({ state: 'UserAlredyExists' });
+            };    
+        } else if(resultOfTokenTime === 'ExictedTime') {
             return res.json({ state: 'UserDidntExists' });
-        } else if (resultOfFunc === 'Exists') {
-            return res.json({ state: 'UserAlredyExists' });
-        };    
-    } else if(resultOfTokenTime === 'ExictedTime') {
-        return res.json({ state: 'UserDidntExists' });
-    } else if(resultOfTokenTime === 'InvalidToken') {
-        return res.json({ state: 'TokenIsInvalid'});
+        } else if(resultOfTokenTime === 'InvalidToken') {
+            return res.json({ state: 'TokenIsInvalid'});
+        };
+    } catch (error) {
+        console.error('/isLogined Rout Have An Error: ', error);
     };
 });
 
 server.post('/registerData', async (req,res) => {
-    const objData = req.body;
-    await insertData(pool, objData.usernameArg, objData.EmailArg, objData.PasswordArg);
+    try {
+        const objData = req.body;
+        await insertData(pool, objData.usernameArg, objData.EmailArg, objData.PasswordArg);
+    } catch (error) {
+        console.error('/registerData Rout Have An Error: ', error);
+    };
 });
 
 server.post('/loginData', async (req,res) => {
-    const dataForLogin = req.body;
-    const username = dataForLogin.loginUsernameArg;
-    const password = dataForLogin.loginPasswordArg;
-    const result = await checkLoginData(pool, username, password);
-    if(result === 'successfullyLogin') {
-        const token = jwt.sign({ username }, secret, { expiresIn: '20' });
-        await insertToken(pool, username, password, token);
-        return res.json({ token: token, pageName: 'mainPage' });
-    } else if(result === 'wrongLogin') {
-        return res.json({ token: 'wrongLoginForToken', pageName: 'Login'});
+    try {
+        const dataForLogin = req.body;
+        const username = dataForLogin.loginUsernameArg;
+        const password = dataForLogin.loginPasswordArg;
+        const result = await checkLoginData(pool, username, password);
+        if(result === 'successfullyLogin') {
+            const token = jwt.sign({ username }, secret, { expiresIn: '30d' });
+            await insertToken(pool, username, password, token);
+            return res.json({ token: token, pageName: 'mainPage' });
+        } else if(result === 'wrongLogin') {
+            return res.json({ token: 'wrongLoginForToken', pageName: 'Login'});
+        };
+    } catch (error) {
+        console.error('/loginData Rout Have An Error: ', error);
     };
+});
+
+server.use((req,res,next) => {
+    const error = new Error('Code Catch Error From Routs!');
+    error.status = 400;
+    next(error);
+});
+
+server.use((err, req, res, next) => {
+    console.error('Error: ', err.message);
+    res.status(err.status || 500).json({
+        success: false,
+        errorMessage: err.message
+    });
 });
 
 server.listen(port, '0.0.0.0', () => {
